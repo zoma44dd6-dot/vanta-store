8636cffba7b15ebf937b8282a70872d02e1cc199
zoma44dd6-dot <zoma44dd6@gmail.com>
2026-08-28 03:03:30 +0300

    Delete next-env.d.ts

--- next-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited
-// See https://nextjs.org/docs/app/api-reference/config/typescript for more information.
